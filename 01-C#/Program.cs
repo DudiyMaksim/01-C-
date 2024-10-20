627cda428c8fee81b1343dbcf5ce2df4de68a609
User <User@DESKTOP-6NPU76D>
2024-10-20 20:51:04 +0300

    Add project files.

---
+++ 01-C#/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+namespace _01_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// task 1
+            //int number = int.Parse(Console.ReadLine());
+            //if (number > 1 && number < 100)
+            //{
+            //    if (number % 3 == 0 && number % 5 == 0)
+            //    {
+            //        Console.WriteLine("Fizz Buzz");
+            //    }
+            //    else if (number % 3 == 1)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //}
+            //else {
+            //    Console.WriteLine("Error: Out_of_range");
+            //}
+            //// task 2
+            //double number1 = double.Parse(Console.ReadLine());
+            //double number2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine(number2/100*number1);
+            //// task 3
+            //int n1 = int.Parse(Console.ReadLine());
+            //int n2 = int.Parse(Console.ReadLine()); 
+            //int n3 = int.Parse(Console.ReadLine());
+            //int n4 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(n1*1000+n2*100+n3*10+n4);
+            //// task 4
+            //int value = int.Parse(Console.ReadLine());
+            //if (!(value / 100000 > 0 && value / 1000000 == 0))
+            //{
+            //    Console.WriteLine("Error: not_six_figures");
+            //}
+
+            //int n1 = int.Parse(Console.ReadLine());
+            //int n2 = int.Parse(Console.ReadLine());
+
+            //double n1p = (value / Math.Pow(10.0, 6 - (double)n1)) % 10;
+            //double n2p = (value / Math.Pow(10.0, 6 - (double)n2)) % 10;
+
+            //double valuenew = value - (int)n1p * Math.Pow(10.0, 6 - n1) + (int)n2p * Math.Pow(10.0, 6 - n1);
+            //valuenew = valuenew - (int)n2p * Math.Pow(10.0, 6 - n2) + (int)n1p * Math.Pow(10.0, 6 - n2);
+
+            //Console.WriteLine(valuenew);
+            //// task 6
+            //string t = Console.ReadLine();
+            //if (t[^1] == 'F')
+            //{
+            //    string numberPart = t.TrimEnd('F');
+            //    int value = int.Parse(numberPart);
+            //    Console.WriteLine((value - 32) * 5 / 9 + "C");
+            //}
+            //else if (t[^1] == 'F')
+            //{
+            //    string numberPart = t.TrimEnd('C');
+            //    int value = int.Parse(numberPart);
+            //    Console.WriteLine((value * 9 / 5) + 32 + "F");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid data");
+            //}
+            ////task 7
+            //int n1 = int.Parse(Console.ReadLine());
+            //int n2 = int.Parse(Console.ReadLine());
+            //for (int i = n1; i < n2; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+        }
+    }
+}
